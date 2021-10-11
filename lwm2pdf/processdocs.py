1c3713811d419dd7e74a12985cca8ed4cfddb762
Danny Elfanbaum <delfanbaum@oreilly.com>
2021-10-11 17:56:45 -0400

    add handling for when asciidoctor is not installed

--- lwm2pdf/processdocs.py
+++ lwm2pdf/processdocs.py
@@ -6,12 +6,39 @@ import markdown2 # type: ignore
 # ---------------------------------------------------------------------
 
 def asciidoc_to_html(fn):
-    print("Running our input through asciidoctor....")
-    result = subprocess.run(['asciidoctor', '-a', "stylesheet!", fn, "-o", "-"], capture_output=True, text=True)
-    if result.stderr == '':    
-        return result.stdout
-    else:
-        raise SystemExit(f'Error: {result.stderr}')
+    print("Running our input through asciidoc conversion....")
+    print("Checking for asciidoctor...")
+    try: 
+        test_asciidoctor = subprocess.run(['asciidoctor', '-v'], 
+                            capture_output=True, text=True)
+        if test_asciidoctor.stderr == '':
+            print("Asciidoctor is present; convering with asciidoctor...") 
+            result = subprocess.run(['asciidoctor', '-a', "stylesheet!", fn, "-o", "-"], capture_output=True, text=True)
+        if result.stderr == '':    
+            return result.stdout
+        else:
+            # raise SystemExit(f'Error: {result.stderr}')
+            print(f'Error: {result.stderr}')
+
+    except FileNotFoundError:
+        print("\n---\nWARNING: Asciidoctor was not found on your PATH.")
+        print("\nWe will proceed with the asciidoc-py converter,")
+        print("but we recommend installing asciidoctor for")
+        print("best results. Some features may not be available.")
+        print("See https://asciidoctor.org/ for more information.\n---")
+        print("\nProceeding with asciidoc-py...")
+        result = subprocess.run(['asciidoc', '-b', 'html5', '-a', 'linkcss', '-a', 'disable-javascript', '-o', '-', fn],
+                                    capture_output=True, text=True)
+        if result.returncode == 0:    
+            html =  result.stdout
+            # remove linked stylesheet....
+            html = html.replace('<link rel="stylesheet" href="./asciidoc.css" type="text/css">', '')
+            return html
+        else:
+            print()
+            # raise SystemExit(f'Error: {result.stderr}')
+            print(f'Error code {result.returncode}: {result.stderr}')                             
+            exit()
 
 def md_to_html(fn):
     print("Running input through markdown conversion....")
