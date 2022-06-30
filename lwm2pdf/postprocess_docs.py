54c02794e6ef4148a4517b8e365cb42282cbd686
Danny Elfanbaum <drelfanbaum@gmail.com>
2022-06-30 16:03:17 -0400

    first cut at quotes

---
+++ lwm2pdf/postprocess_docs.py
@@ -0,0 +1,75 @@
+import bs4
+import re
+
+# TO DO: Can we iterate over .strings instead?
+
+
+def add_smart_quotes_pairs(string):
+    """ add paired quotes """
+    r = re.compile(r"'(.*?)'")
+    s_quoted_str = re.sub(r, r'‘\1’', string)
+    r = re.compile(r'"(.*?)"')
+    quoted_str = re.sub(r, r'“\1”', s_quoted_str)
+    return quoted_str
+
+
+def add_double_quotes_to_children(parent):
+    """ recursively add quotes to child strings """
+    unpaired = False
+    for child in parent.children:
+        if child.name != "code":
+            if child.string:
+                quoted_str = add_smart_quotes_pairs(child.string)
+                # handle quotes around children
+                if (
+                    quoted_str == child.string and
+                    child.string.find('"') > -1
+                ):
+                    if not unpaired:
+                        quoted_str = child.string.replace('"', '“')
+                        unpaired = True
+                    elif unpaired:
+                        quoted_str = child.string.replace('"', '”')
+                        unpaired = False
+
+                child.string.replace_with(quoted_str)
+            else:
+                add_double_quotes_to_children(child)
+
+
+def add_single_quotes_to_children(parent):
+    """ recursively add quotes to child strings """
+    unpaired = False
+    for child in parent.children:
+        if child.string:
+            quoted_str = add_smart_quotes_pairs(child.string)
+            # handle quotes around children
+            if (
+                quoted_str == child.string and
+                child.string.find("'") > -1
+            ):
+                if not unpaired:
+                    quoted_str = child.string.replace("'", "‘")
+                    unpaired = True
+                elif unpaired:
+                    quoted_str = child.string.replace("'", "’")
+                    unpaired = False
+
+            child.string.replace_with(quoted_str)
+        else:
+            add_single_quotes_to_children(child)
+
+
+def postprocess_html_quotes(html):
+    """ add smart double quotes """
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    paras = soup.find_all('p')
+    for p in paras:
+        p_str = p.string
+        if p.string:  # no inner elements
+            quoted_str = add_smart_quotes_pairs(p_str)
+            p_str.replace_with(quoted_str)
+        else:
+            add_double_quotes_to_children(p)
+            add_single_quotes_to_children(p)
+    return str(soup)
---
+++ tests/test_quotes.py
@@ -0,0 +1,59 @@
+from lwm2pdf.postprocess_docs import postprocess_html_quotes
+
+
+def test_basic_dbl_quote():
+    """ happy path test case for double quotes """
+    start = '<p>Some "quoted text"</p>'
+    result = postprocess_html_quotes(start)
+    expected = '<p>Some “quoted text”</p>'
+    assert result == expected
+
+
+def test_basic_sgl_quote():
+    """ happy path test case for single quotes """
+    start = "<p>Some 'quoted text'</p>"
+    result = postprocess_html_quotes(start)
+    expected = '<p>Some ‘quoted text’</p>'
+    assert result == expected
+
+
+def test_inside_quote_us():
+    """ happy path test case for single quotes inside double """
+    start = "<p>Some \"'quoted text'\"</p>"
+    result = postprocess_html_quotes(start)
+    expected = '<p>Some “‘quoted text’”</p>'
+    assert result == expected
+
+
+def test_inner_elements_simple():
+    """ test for elements inside a quote thing """
+    start = """
+            <p>Some "quoted <em>emphasized</em> text"</p>
+            <p>Some 'single <strong>stuff</strong>'</p>
+            """
+    result = postprocess_html_quotes(start)
+    expected = """
+<p>Some “quoted <em>emphasized</em> text”</p>
+<p>Some ‘single <strong>stuff</strong>’</p>
+"""
+    assert result == expected
+
+
+def test_inner_elements_more_complex():
+    """ test for elements inside a quote thing in a harder context"""
+    start = """<p>Some "quoted <em>emphasized</em> text"
+Some 'single <strong>stuff</strong>'</p>
+"""
+    result = postprocess_html_quotes(start)
+    expected = """<p>Some “quoted <em>emphasized</em> text”
+Some ‘single <strong>stuff</strong>’</p>
+"""
+    assert result == expected
+
+
+def test_dont_smart_quote_code():
+    """ ensure we're not screwing with code """
+    start = '<p>Some <code>"String"</code></p>'
+    result = postprocess_html_quotes(start)
+    expected = '<p>Some <code>"String"</code></p>'
+    assert result == expected
